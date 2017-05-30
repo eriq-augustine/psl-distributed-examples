3fc9c6f589c4bc461191d1890ad28334921263f5
Eriq Augustine <eriq.augustine@gmail.com>
2017-05-30 13:55:55 -0700

    Added ability to specify workers on the command line.

--- friendship/src/main/java/org/linqs/psl/example/Friendship.groovy
+++ friendship/src/main/java/org/linqs/psl/example/Friendship.groovy
@@ -39,150 +39,150 @@ import java.util.List;
 import java.util.Map;
 
 public class Friendship {
-	private static final String PARTITION_OBSERVATIONS = "observations";
-	private static final String PARTITION_TARGETS = "targets";
-	private static final String PARTITION_TRUTH = "truth";
+   private static final String PARTITION_OBSERVATIONS = "observations";
+   private static final String PARTITION_TARGETS = "targets";
+   private static final String PARTITION_TRUTH = "truth";
 
    private static final String ID = "friendship";
 
-	private Logger log;
-	private DataStore ds;
-	private PSLConfig config;
-	private PSLModel model;
+   private Logger log;
+   private DataStore ds;
+   private PSLConfig config;
+   private PSLModel model;
 
-	/**
-	 * Class for config variables
-	 */
-	private class PSLConfig {
-		public ConfigBundle cb;
+   /**
+    * Class for config variables
+    */
+   private class PSLConfig {
+      public ConfigBundle cb;
 
-		public String dbPath;
-		public String dataPath;
-		public String outputPath;
+      public String dbPath;
+      public String dataPath;
+      public String outputPath;
 
-		public boolean sqPotentials;
+      public boolean sqPotentials;
 
-		public distributed;
-		public master;
+      public distributed;
+      public master;
 
-		public PSLConfig(ConfigBundle cb) {
-			this.cb = cb;
+      public PSLConfig(ConfigBundle cb) {
+         this.cb = cb;
 
-			distributed = cb.getBoolean('distributed', false);
-			master = cb.getBoolean('master', false);
+         distributed = cb.getBoolean('distributed', false);
+         master = cb.getBoolean('master', false);
 
          String suffix = distributed ? (master ? "master" : "worker") : "standalone";
-			dbPath = Paths.get(cb.getString('experiment.dbpath', '/tmp'), ID + "_" + suffix);
-			dataPath = cb.getString('experiment.data.path', 'data');
-			outputPath = cb.getString('experiment.output.outputdir', 'output');
+         dbPath = Paths.get(cb.getString('experiment.dbpath', '/tmp'), ID + "_" + suffix);
+         dataPath = cb.getString('experiment.data.path', 'data');
+         outputPath = cb.getString('experiment.output.outputdir', 'output');
 
          sqPotentials = true;
-		}
-	}
+      }
+   }
 
-	public Friendship(ConfigBundle cb) {
-		log = LoggerFactory.getLogger(this.class);
-		config = new PSLConfig(cb);
-		ds = new RDBMSDataStore(new H2DatabaseDriver(Type.Disk, Paths.get(config.dbPath, ID).toString(), true), cb);
-		model = new PSLModel(this, ds);
-	}
+   public Friendship(ConfigBundle cb) {
+      log = LoggerFactory.getLogger(this.class);
+      config = new PSLConfig(cb);
+      ds = new RDBMSDataStore(new H2DatabaseDriver(Type.Disk, Paths.get(config.dbPath, ID).toString(), true), cb);
+      model = new PSLModel(this, ds);
+   }
 
-	/**
-	 * Defines the logical predicates used in this model
-	 */
-	private void definePredicates() {
-		model.add(
+   /**
+    * Defines the logical predicates used in this model
+    */
+   private void definePredicates() {
+      model.add(
          predicate: "Similar",
          types: [ConstantType.UniqueID, ConstantType.UniqueID]
       );
 
-		model.add(
+      model.add(
          predicate: "Friends",
          types: [ConstantType.UniqueID, ConstantType.UniqueID]
       );
 
-		model.add(
+      model.add(
          predicate: "Block",
          types: [ConstantType.UniqueID, ConstantType.UniqueID]
       );
-	}
+   }
 
-	/**
-	 * Defines the rules for this model, optionally including transitivty and
-	 * symmetry based on the PSLConfig options specified
-	 */
-	private void defineRules() {
-		log.info("Defining model rules");
+   /**
+    * Defines the rules for this model, optionally including transitivty and
+    * symmetry based on the PSLConfig options specified
+    */
+   private void defineRules() {
+      log.info("Defining model rules");
 
-		model.add(
-			rule: "Block(P1, A) & Block(P2, A) & Similar(P1, P2) & P1 != P2 -> Friends(P1, P2)",
-			squared: config.sqPotentials,
-			weight : 10
-		);
+      model.add(
+         rule: "Block(P1, A) & Block(P2, A) & Similar(P1, P2) & P1 != P2 -> Friends(P1, P2)",
+         squared: config.sqPotentials,
+         weight : 10
+      );
 
-		model.add(
-			rule: "Block(P1, A) & Block(P2, A) & Block(P3, A) & Friends(P1, P2) & Friends(P2, P3) & P1 != P2 & P2 != P3 & P1 != P3 -> Friends(P1, P3)",
-			squared: config.sqPotentials,
-			weight : 10
-		);
+      model.add(
+         rule: "Block(P1, A) & Block(P2, A) & Block(P3, A) & Friends(P1, P2) & Friends(P2, P3) & P1 != P2 & P2 != P3 & P1 != P3 -> Friends(P1, P3)",
+         squared: config.sqPotentials,
+         weight : 10
+      );
 
-		model.add(
-			rule: "Block(P1, A) & Block(P2, A) & Friends(P1, P2) & P1 != P2 -> Friends(P2, P1)",
-			squared: config.sqPotentials,
-			weight : 10
-		);
+      model.add(
+         rule: "Block(P1, A) & Block(P2, A) & Friends(P1, P2) & P1 != P2 -> Friends(P2, P1)",
+         squared: config.sqPotentials,
+         weight : 10
+      );
 
       // TODO(eriq): This is a problem! It will ground all, but we don't want that.
-		model.add(
-			rule: "!Friends(P1, P2)",
-			squared: config.sqPotentials,
-			weight : 10
-		);
+      model.add(
+         rule: "!Friends(P1, P2)",
+         squared: config.sqPotentials,
+         weight : 10
+      );
 
-		log.debug("model: {}", model);
-	}
+      log.debug("model: {}", model);
+   }
 
-	/**
-	 * Load data from text files into the DataStore. Three partitions are defined
-	 * and populated: observations, targets, and truth.
-	 * Observations contains evidence that we treat as background knowledge and
-	 * use to condition our inferences
-	 * Targets contains the inference targets - the unknown variables we wish to infer
-	 * Truth contains the true values of the inference variables and will be used
-	 * to evaluate the model's performance
-	 */
-	private void loadData(Partition obsPartition, Partition targetsPartition, Partition truthPartition) {
-		log.info("Loading data into database");
+   /**
+    * Load data from text files into the DataStore. Three partitions are defined
+    * and populated: observations, targets, and truth.
+    * Observations contains evidence that we treat as background knowledge and
+    * use to condition our inferences
+    * Targets contains the inference targets - the unknown variables we wish to infer
+    * Truth contains the true values of the inference variables and will be used
+    * to evaluate the model's performance
+    */
+   private void loadData(Partition obsPartition, Partition targetsPartition, Partition truthPartition) {
+      log.info("Loading data into database");
 
-		Inserter inserter = ds.getInserter(Similar, obsPartition);
-		InserterUtils.loadDelimitedDataTruth(inserter, Paths.get(config.dataPath, "similar_obs.txt").toString());
+      Inserter inserter = ds.getInserter(Similar, obsPartition);
+      InserterUtils.loadDelimitedDataTruth(inserter, Paths.get(config.dataPath, "similar_obs.txt").toString());
 
       /*
-		inserter = ds.getInserter(Friends, obsPartition);
-		InserterUtils.loadDelimitedData(inserter, Paths.get(config.dataPath, "friends_obs.txt").toString());
+      inserter = ds.getInserter(Friends, obsPartition);
+      InserterUtils.loadDelimitedData(inserter, Paths.get(config.dataPath, "friends_obs.txt").toString());
       */
 
-		inserter = ds.getInserter(Friends, targetsPartition);
-		InserterUtils.loadDelimitedData(inserter, Paths.get(config.dataPath, "friends_targets.txt").toString());
+      inserter = ds.getInserter(Friends, targetsPartition);
+      InserterUtils.loadDelimitedData(inserter, Paths.get(config.dataPath, "friends_targets.txt").toString());
 
-		inserter = ds.getInserter(Friends, truthPartition);
-		InserterUtils.loadDelimitedDataTruth(inserter, Paths.get(config.dataPath, "friends_truth.txt").toString());
+      inserter = ds.getInserter(Friends, truthPartition);
+      InserterUtils.loadDelimitedDataTruth(inserter, Paths.get(config.dataPath, "friends_truth.txt").toString());
 
       if (!config.distributed) {
          // Use location as block.
          inserter = ds.getInserter(Block, obsPartition);
          InserterUtils.loadDelimitedData(inserter, Paths.get(config.dataPath, "location_obs.txt").toString());
       }
-	}
+   }
 
-	/**
-	 * Run inference to infer the unknown Knows relationships between people.
-	 */
-	private void runInference(Partition obsPartition, Partition targetsPartition) {
-		log.info("Starting inference");
+   /**
+    * Run inference to infer the unknown Knows relationships between people.
+    */
+   private void runInference(Partition obsPartition, Partition targetsPartition) {
+      log.info("Starting inference");
 
-		Date infStart = new Date();
-		HashSet closed = new HashSet<StandardPredicate>([Lived, Likes]);
+      Date infStart = new Date();
+      HashSet closed = new HashSet<StandardPredicate>([Lived, Likes]);
 
       if (!config.distributed) {
          Database inferDB = ds.getDatabase(targetsPartition, closed, obsPartition);
@@ -204,53 +204,53 @@ public class Friendship {
          }
       }
 
-		log.info("Finished inference in {}", TimeCategory.minus(new Date(), infStart));
-	}
+      log.info("Finished inference in {}", TimeCategory.minus(new Date(), infStart));
+   }
 
-	/**
-	 * Writes the output of the model into a file
-	 */
-	private void writeOutput(Partition targetsPartition) {
-		Database resultsDB = ds.getDatabase(targetsPartition);
-		PrintStream ps = new PrintStream(new File(Paths.get(config.outputPath, "friends_infer.txt").toString()));
-		AtomPrintStream aps = new DefaultAtomPrintStream(ps);
-		Set atomSet = Queries.getAllAtoms(resultsDB, Friends);
-		for (Atom a : atomSet) {
-			aps.printAtom(a);
-		}
+   /**
+    * Writes the output of the model into a file
+    */
+   private void writeOutput(Partition targetsPartition) {
+      Database resultsDB = ds.getDatabase(targetsPartition);
+      PrintStream ps = new PrintStream(new File(Paths.get(config.outputPath, "friends_infer.txt").toString()));
+      AtomPrintStream aps = new DefaultAtomPrintStream(ps);
+      Set atomSet = Queries.getAllAtoms(resultsDB, Friends);
+      for (Atom a : atomSet) {
+         aps.printAtom(a);
+      }
 
-		aps.close();
-		ps.close();
-		resultsDB.close();
-	}
+      aps.close();
+      ps.close();
+      resultsDB.close();
+   }
 
-	/**
-	 * Run statistical evaluation scripts to determine the quality of the inferences
-	 * relative to the defined truth.
-	 */
-	private void evalResults(Partition targetsPartition, Partition truthPartition) {
-		Database resultsDB = ds.getDatabase(targetsPartition, [Friends] as Set);
-		Database truthDB = ds.getDatabase(truthPartition, [Friends] as Set);
-		DiscretePredictionComparator dpc = new DiscretePredictionComparator(resultsDB);
-		ContinuousPredictionComparator cpc = new ContinuousPredictionComparator(resultsDB);
-		dpc.setBaseline(truthDB);
-		//	 dpc.setThreshold(0.99);
-		cpc.setBaseline(truthDB);
-		DiscretePredictionStatistics stats = dpc.compare(Friends);
-		double mse = cpc.compare(Friends);
-		log.info("MSE: {}", mse);
-		log.info("Accuracy {}, Error {}",stats.getAccuracy(), stats.getError());
-		log.info(
-				"Positive Class: precision {}, recall {}",
-				stats.getPrecision(DiscretePredictionStatistics.BinaryClass.POSITIVE),
-				stats.getRecall(DiscretePredictionStatistics.BinaryClass.POSITIVE));
-		log.info("Negative Class Stats: precision {}, recall {}",
-				stats.getPrecision(DiscretePredictionStatistics.BinaryClass.NEGATIVE),
-				stats.getRecall(DiscretePredictionStatistics.BinaryClass.NEGATIVE));
+   /**
+    * Run statistical evaluation scripts to determine the quality of the inferences
+    * relative to the defined truth.
+    */
+   private void evalResults(Partition targetsPartition, Partition truthPartition) {
+      Database resultsDB = ds.getDatabase(targetsPartition, [Friends] as Set);
+      Database truthDB = ds.getDatabase(truthPartition, [Friends] as Set);
+      DiscretePredictionComparator dpc = new DiscretePredictionComparator(resultsDB);
+      ContinuousPredictionComparator cpc = new ContinuousPredictionComparator(resultsDB);
+      dpc.setBaseline(truthDB);
+      //    dpc.setThreshold(0.99);
+      cpc.setBaseline(truthDB);
+      DiscretePredictionStatistics stats = dpc.compare(Friends);
+      double mse = cpc.compare(Friends);
+      log.info("MSE: {}", mse);
+      log.info("Accuracy {}, Error {}",stats.getAccuracy(), stats.getError());
+      log.info(
+            "Positive Class: precision {}, recall {}",
+            stats.getPrecision(DiscretePredictionStatistics.BinaryClass.POSITIVE),
+            stats.getRecall(DiscretePredictionStatistics.BinaryClass.POSITIVE));
+      log.info("Negative Class Stats: precision {}, recall {}",
+            stats.getPrecision(DiscretePredictionStatistics.BinaryClass.NEGATIVE),
+            stats.getRecall(DiscretePredictionStatistics.BinaryClass.NEGATIVE));
 
-		resultsDB.close();
-		truthDB.close();
-	}
+      resultsDB.close();
+      truthDB.close();
+   }
 
    /**
     * Take the blocks and assign them to workers.
@@ -313,65 +313,77 @@ public class Friendship {
       return blocks;
    }
 
-	public void run() {
-		log.info("Running experiment...");
+   public void run() {
+      log.info("Running experiment...");
 
-		Partition obsPartition = ds.getPartition(PARTITION_OBSERVATIONS);
-		Partition targetsPartition = ds.getPartition(PARTITION_TARGETS);
-		Partition truthPartition = ds.getPartition(PARTITION_TRUTH);
+      Partition obsPartition = ds.getPartition(PARTITION_OBSERVATIONS);
+      Partition targetsPartition = ds.getPartition(PARTITION_TARGETS);
+      Partition truthPartition = ds.getPartition(PARTITION_TRUTH);
 
-		definePredicates();
-		defineRules();
-		loadData(obsPartition, targetsPartition, truthPartition);
-		runInference(obsPartition, targetsPartition);
+      definePredicates();
+      defineRules();
+      loadData(obsPartition, targetsPartition, truthPartition);
+      runInference(obsPartition, targetsPartition);
 
-		if (!config.distributed || config.master) {
+      if (!config.distributed || config.master) {
          writeOutput(targetsPartition);
          evalResults(targetsPartition, truthPartition);
       }
 
-		ds.close();
-	}
+      ds.close();
+   }
 
-	/**
-	 * Parse the command line options and populate them into a ConfigBundle
-	 * Currently the only argument supported is the path to the data directory
-	 * @param args - the command line arguments provided during the invocation
-	 * @return - a ConfigBundle populated with options from the command line options
-	 */
-	public static ConfigBundle populateConfigBundle(String[] args) {
-		ConfigBundle cb = ConfigManager.getManager().getBundle(ID);
+   /**
+    * Parse the command line options and populate them into a ConfigBundle
+    * Currently the only argument supported is the path to the data directory
+    * @param args - the command line arguments provided during the invocation
+    * @return - a ConfigBundle populated with options from the command line options
+    */
+   public static ConfigBundle populateConfigBundle(String[] args) {
+      ConfigBundle cb = ConfigManager.getManager().getBundle(ID);
 
       cb.setProperty('distributed', false);
       cb.setProperty('master', false);
 
       for (int i = 0; i < args.length; i++) {
-         if (args[i].equals("--worker")) {
+         if (args[i].equals("--help") || args[i].equals("-h")) {
+            printUsage();
+            System.exit(0);
+         } else if (args[i].equals("--worker")) {
             cb.setProperty('distributed', true);
             cb.setProperty('master', false);
          } else if (args[i].equals("--master")) {
             cb.setProperty('distributed', true);
             cb.setProperty('master', true);
+         } else if (args[i].startsWith("--")) {
+            System.err.println("Unknown argument: [" + args[i] + "]");
+            printUsage();
+            System.exit(1);
          } else {
-            throw new RuntimeException("Unknown argument: [" + args[i] + "]");
+            cb.addProperty('distributedmpeinference.workers', args[i]);
          }
       }
 
-		// TEST(eriq)
-		cb.addProperty('distributedmpeinference.workers', 'sunset.soe.ucsc.edu:12345');
-		// cb.addProperty('distributedmpeinference.workers', 'localhost:12345');
-		cb.addProperty('distributedmpeinference.workers', 'eriqs-shit.com:12345');
+      return cb;
+   }
 
-		return cb;
-	}
+   private static void printUsage() {
+      System.out.println("USAGE: java " + Friendship.getClass().getName() + " [OPTIONS] workerAddress ...");
+      System.out.println("Options:");
+      System.out.println("   --help | -h -- print this prompt and exit");
+      System.out.println("   --worker -- make this instance a worker");
+      System.out.println("   --master -- make this instance a master");
+      System.out.println("");
+      System.out.println("All additional values are treated as worker addresses (will be added to 'distributedmpeinference.workers'");
+   }
 
-	/**
-	 * Run this model from the command line
-	 * @param args - the command line arguments
-	 */
-	public static void main(String[] args) {
-		ConfigBundle configBundle = populateConfigBundle(args);
-		Friendship lp = new Friendship(configBundle);
-		lp.run();
-	}
+   /**
+    * Run this model from the command line
+    * @param args - the command line arguments
+    */
+   public static void main(String[] args) {
+      ConfigBundle configBundle = populateConfigBundle(args);
+      Friendship lp = new Friendship(configBundle);
+      lp.run();
+   }
 }
