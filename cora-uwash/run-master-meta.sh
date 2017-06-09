a5b3f791d201adbec57b6625d35d4236dc5a2ef5
Nikhil Kini <nkini@ucsc.edu>
2017-06-09 09:46:09 -0700

    master's db is in /tmp/corauwash_master, not /tmp/corauwash_worker

--- cora-uwash/run-master-meta.sh
+++ cora-uwash/run-master-meta.sh
@@ -1,2 +1,2 @@
-cp /tmp/corauwash_worker/corauwash-preloaded.mv.db /tmp/corauwash_worker/corauwash-exp$1.mv.db
-./run-master.sh --runid=$1 2>&1 | tee -a logs/Expt$1-DistCoraUWash.master.$2.log
+cp /tmp/corauwash_master/corauwash-preloaded.mv.db /tmp/corauwash_master/corauwash-exp$1.mv.db
+./run-master.sh --runid=$1 2>&1 | tee -a logs/Expt$1-DistCoraUWash.master.sozopol.log
